2138f7f8fffbd7a4cf861baaed8bdfe3c99f0921
Torge <torge.stubbe@googlemail.com>
2024-10-24 20:47:11 +0200

    main page init

---
+++ src/lib/component/Topbar.svelte
@@ -0,0 +1,14 @@
+<style>
+    h1 {
+        font-family: 'Bitter', sans-serif;
+        font-size: 4.5em;
+        font-weight: 700;
+        margin-bottom: 0;
+    }
+
+</style>
+
+<div class="row justify-between">
+    <h1>Moin.</h1>
+</div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,59 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import Topbar from '$lib/component/Topbar.svelte';
+  import me from '$lib/asset/me.jpeg';
+</script>
+  
+<style>
+  h2 {
+      font-size: 2.2em;
+      font-weight: 700;
+      letter-spacing: -1.5px;
+  }
+
+  p {
+      font-size: 1.3em;
+      font-weight: 300;
+      line-height: 1.4;
+      max-width: 28em;
+      mask-position: topt;
+  }
+
+  #me{
+    mask-image: url('$lib/asset/scribble.png');
+    mask-size: 650px;
+    mask-position: right;
+    mask-repeat: no-repeat;
+    width: 600px;
+    height: auto;
+  }
+
+  #me img{
+    object-position: top;
+    object-fit: fill;
+  }
+
+
+</style>
+
+
+  <Topbar></Topbar>
+  <div class="row justify-between">
+
+
+  <div>
+    <h2>My name is Torge Stubbe.</h2>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+  </div>
+  
+  <!-- <div id="me">
+    <img  src={me} alt="" srcset="">
+  </div> -->
+</div>

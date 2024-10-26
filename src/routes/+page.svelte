24cbc9cb24a4b204bc8a13973771231338a90f34
Torge <torge.stubbe@googlemail.com>
2024-10-27 00:31:25 +0200

    break layout on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,18 +4,19 @@
 </script>
   
 <style>
-  h2 {
-      font-size: 2.2em;
-      font-weight: 700;
-      letter-spacing: -1.5px;
+  h2{
+    font-size: 2.2em;
+    font-weight: 700;
+    letter-spacing: -1.5px;
   }
 
-  p {
-      font-size: 1.3em;
-      font-weight: 300;
-      line-height: 1.4;
-      max-width: 28em;
-      mask-position: topt;
+  p{
+    font-size: 1.3em;
+    font-weight: 300;
+    line-height: 1.4;
+    max-width: 28em;
+    mask-position: topt;
+    text-align: justify;
   }
 
   #me{
@@ -25,13 +26,13 @@
 
 </style>
 
-  <div class="tw-container tw-mx-auto">
+  <div class="tw-container tw-mx-auto tw-px-44">
 
   <Topbar></Topbar>
-  <div class="tw-row tw-justify-between">
+  <div class="tw-flex tw-flex-col md:tw-flex-row tw-justify-between">
 
 
-  <div class="tw-w-[50%]">
+  <div class="md:tw-w-[50%]">
     <h2>My name is Torge Stubbe.</h2>
     <p>
       I am a professionally trained software developer based in northern Germany, self-identified as a Full Stack developer. I currently work at <a href="https://fricke.de">FRICKE Group</a>, where I maintain the <a href="https://granit-parts.com">granit-parts</a> B2B webshop. Most of my  private work is open source and publicly available on GitHub.
@@ -44,7 +45,7 @@
   </div>
 
   <!-- <Polaroid src={me} caption="me"></Polaroid> -->
-  <div class="tw-col tw-items-center tw-w-[50%]">
+  <div class="tw-col tw-items-center md:tw-w-[50%]">
     <img id="me" src={me} alt="" srcset="">
   </div>
 

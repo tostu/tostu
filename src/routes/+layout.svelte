6a7e72719d839310f8cde465d5a65f9d8cae75f7
Torge <torge.stubbe@googlemail.com>
2024-10-27 16:22:45 +0100

    sso improvements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,4 +5,9 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>Torge Stubbe</title>
+	<meta name="description" content="My name is Torge Stubbe. I'm a Full Stack software developer from northern Germany.">
+</svelte:head>
+
 {@render children()}

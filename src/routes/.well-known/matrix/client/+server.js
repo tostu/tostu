980841cfd099ec5c9a42399b740d0612f972e3ee
tostu <torge.stubbe@googlemail.com>
2025-04-29 01:43:13 +0200

    Reorder fields in Matrix client discovery response

--- src/routes/.well-known/matrix/client/+server.js
+++ src/routes/.well-known/matrix/client/+server.js
@@ -2,13 +2,13 @@ export const prerender = true;
 
 export function GET() {
 	const data = {
+		'm.homeserver': { base_url: 'https://matrix.tostu.me' },
 		'org.matrix.msc4143.rtc_foci': [
 			{
 				type: 'livekit',
 				livekit_service_url: 'https://livekit-jwt.call.matrix.org'
 			}
-		],
-		'm.homeserver': { base_url: 'https://matrix.tostu.me' }
+		]
 	};
 
 	return new Response(JSON.stringify(data), {
